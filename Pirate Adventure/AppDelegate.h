0ec7fdf480402ebdd804e2e017403f6b7386e7b9
Andy Lee <andylee@Andys-MacBook-Air.local>
2015-01-19 16:58:51 +1000

    Initial Commit

---
+++ Pirate Adventure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pirate Adventure
+//
+//  Created by Andy Lee on 19/01/2015.
+//  Copyright (c) 2015 Appfinity Creations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pirate Adventure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pirate Adventure
+//
+//  Created by Andy Lee on 19/01/2015.
+//  Copyright (c) 2015 Appfinity Creations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
